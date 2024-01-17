92f2c5f9939c3ab3e13fe60cd436170b93125c0f
Ashika <ashibasama@gmail.com>
2024-01-17 17:50:21 +0900

    includeでパーシャルを読みこむ

--- app.js
+++ app.js
@@ -27,50 +27,13 @@ const getFromClient = (request, response) => {
             title: "Index",
             content: msg,
             data: data,
+            filename: 'data_item'
         });
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.write(content);
         response.end();
     }
-
-    //otherのアクセス処理
-    const response_other = (request, response) => {
-        var msg = "これがOtherページですねん"
-
-        //Postアクセス時
-        if (request.method == 'POST') {
-            var body = '';
-            //データ受信のイベント処理
-            request.on('data', (data) => {
-                body += data;
-            });
-
-            //データ受信終了のイベント処理
-            request.on('end', () => {
-                var post_data = qs.parse(body);
-                msg += 'あなたは、「' + post_data.msg +'」と書きました。';
-                var content = ejs.render(other_page, {
-                    title: "Other",
-                    content: msg,
-                });
-                response.writeHead(200, { 'Content-Type': 'text/html' });
-                response.write(content);
-                response.end();
-            });
-
-            //Getアクセスの処理 
-        } else {
-            var msg = "ページがありませんよ？"
-            var content = ejs.render(other_page, {
-                title: "Other",
-                content: msg,
-            });
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(content);
-            response.end();
-        }
-    }
-
+    
     switch (url_parts.pathname) {
 
         case '/':
